8d994d4fdb643af03628d4cc28f944d88ceddf23
mariaf494 <mariaf494@gmail.com>
2020-02-08 15:58:03 -0500

    Agregar css para el sitio y plantillas para ver el detalle de los posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    url('', views.post_list, name='post_list'),
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
